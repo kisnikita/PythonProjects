423d17b9b651ccd15a6b881d71a5544d05ed2f77
nikitakislov <kisloff.nickita@yandex.ru>
2023-02-20 14:35:22 +0300

    First

---
+++ isj_proj7_xkislo00.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import collections
+
+my_counter = collections.Counter()
+
+
+def log_and_count(key=None, counts=None):
+    def log_and_count_decorator(func):
+        def inner(*args, **kwargs):
+            print(f"called {func.__name__} with {args} and {kwargs}")
+            if counts is not None:
+                counts.update({key if key is not None else func.__name__: 1})
+
+        return inner
+
+    return log_and_count_decorator
